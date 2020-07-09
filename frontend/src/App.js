3545e954b42ef4be21c60386578ded60012e373d
Suraj Karki <suraj.karki500@gmail.com>
2020-07-09 15:26:05 +0545

    Created Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Suspense } from "react";
+import "./App.css";
 
-import Navbar from './components/UI/Navbar/NavBar';
+import Navbar from "./components/UI/Navbar/NavBar";
+import Footer from "./components/UI/Footer/Footer";
 
 const App = () => {
   return (
-    <div>
-    <Navbar />
-      Hello
-    </div>
-  )
-}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Navbar />
+      </div>
+      <Footer />
+    </Suspense>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/UI/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright © 2020 by M-DB. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
