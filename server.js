58364420d0d9963cc8c772da13cab0f90ca85f1a
Suraj Karki <suraj.karki500@gmail.com>
2020-07-15 01:11:18 +0545

    heroku deployment

--- server.js
+++ server.js
@@ -42,11 +42,6 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   // All the javascript and css files will be read and served from this folder
   app.use(express.static("frontend/build"));
-
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-  });
 }
 
 app.use('/api/users', userRouter);
